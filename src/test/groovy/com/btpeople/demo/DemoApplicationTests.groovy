4d026c075ace4eab3c7b8c11222a5240e44336c8
Soohan Yang <movingmt@gmail.com>
2020-08-20 12:24:43 +0900

    Create Spring Boot Project

---
+++ src/test/groovy/com/btpeople/demo/DemoApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.btpeople.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
